0790faa552ad43885a115ac07be636acf57d7cb8
Anthony-Villamar <138512747+Anthony-Villamar@users.noreply.github.com>
2025-07-04 11:17:47 -0500

    Update api.php

--- pedidos/routes/api.php
+++ pedidos/routes/api.php
@@ -7,5 +7,8 @@ use App\Http\Controllers\PedidoController;
 
 Route::post('/pedidos', [PedidoController::class, 'crear']);
 Route::get('/pedidos/{id_pedido}', [PedidoController::class, 'consultarPedido']);
+Route::get('/ping', function () {
+  return response()->json(['mensaje' => 'Pedidos UP']);
+});
 
 Route::get('/pedidos/usuario/{usuario_id}', [PedidoController::class, 'listarPorUsuario']);
