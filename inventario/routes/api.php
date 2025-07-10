83fcaf8d26de0f01b97e76053295faa620651d21
Anthony-Villamar <138512747+Anthony-Villamar@users.noreply.github.com>
2025-07-10 15:53:25 -0500

    Update api.php

--- inventario/routes/api.php
+++ inventario/routes/api.php
@@ -6,10 +6,17 @@ use App\Http\Controllers\ProductoController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//registrar pedido
 Route::post('/productos', [ProductoController::class, 'registrar']);
 Route::get('/productos', [ProductoController::class, 'listar']);
+//consultar disponibilidad de productos
 Route::get('/productos/{codigo_producto}', [ProductoController::class, 'consultar']);
+#actualizar stock
 Route::put('/productos/{codigo_producto}/existencias', [ProductoController::class, 'actualizarExistencias']);
+#generar alerta
 Route::get('/alerta-stock', [ProductoController::class, 'alertaStock']);
+#reabastecer producto
 Route::put('/productos/{codigo_producto}/reabastecer', [ProductoController::class, 'reabastecer']);
+#actualizar producto
 Route::put('/productos/{codigo_producto}', [ProductoController::class, 'actualizar']);
